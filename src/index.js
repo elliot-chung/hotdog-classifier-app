2f8358d07c1fa9ae89c2b81a8dbe6906e00dab70
Elliot Chung <ec2018@gmail.com>
2022-01-05 00:02:50 -0800

    Changed code to be asyncronous

--- src/index.js
+++ src/index.js
@@ -6,36 +6,51 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
-const model = loadModel();
+let model = loadModel();
 
 app.listen(
     PORT,
     () => console.log(`it's alive on http://localhost:${PORT}`)
 )
 
-app.get('/evaluate/test', async (req, res)=>{
-    const imageBuffer = fs.readFileSync('uploads/1f37da6c0ccecc1f7c02a887620c6b94.jpeg')
-
-    const img = tf.node.decodeImage(imageBuffer)
-    const resized = tf.image.resizeBilinear(img, [640,480])
-    const casted = resized.cast('int32')
-    const expanded = casted.expandDims(4)
-    const obj = await (await model).executeAsync(expanded)
-
-    res.send(obj)
-});
+app.get('/evaluate/test', (req, res)=>{
+    loadImage('uploads/1f37da6c0ccecc1f7c02a887620c6b94.jpeg', (image)=>{
+        transformImage(image, (transImage)=>{
+            makePrediction(transImage, (prediction)=>{
+                res.send(prediction)
+            })
+        })
+    })
+})
 
 app.post('/evaluate/fileUpload', upload.single('evaluate'), (req, res)=>{
     res.status(200).send(req.file)
 });
 
+function loadImage(path, callback) {
+    fs.readFile(path, (err, imageBuffer)=>{
+        const image = tf.node.decodeImage(imageBuffer)
+        callback(image);
+    })
+}
+
+function transformImage(image, callback) {
+    const resized = tf.image.resizeBilinear(image, [640,480])
+    const casted = resized.cast('int32')
+    const expanded = casted.expandDims(0)
+    callback(expanded)
+}
+
+function makePrediction(transImage, callback) {
+    model.then((net)=>{
+        net.executeAsync(transImage).then((prediction)=>{
+            callback(prediction)
+        })
+    })
+}
+
 async function loadModel () {
     const handler = tf.io.fileSystem("model/model.json")
-    try {
-        const mdl = await tf.loadGraphModel(handler);
-        return mdl;
-    } catch {
-        throw 'Could Not Load Model';
-    }
+    return await tf.loadGraphModel(handler);
 }
 
