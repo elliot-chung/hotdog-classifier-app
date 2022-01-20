1c3e53b752a9329280ba6ba991e2755bc3e8cd04
Elliot Chung <ec2018@gmail.com>
2022-01-19 21:38:56 -0800

    conversion to promises

--- src/index.js
+++ src/index.js
@@ -42,25 +42,27 @@ app.post('/upload', upload.single('evaluate'), (req, res)=>{
 })
 
 app.get('/test', (req, res)=>{
-    loadImage('uploads/diranchor.jpg', (image)=>{
-        transformImage(image, (transImage)=>{
-            makePrediction(transImage, (prediction)=>{
-                formatPrediction(prediction, (resObj)=>{
-                    res.send(resObj)
-                })
-            })
-        })
-    })
+    loadImage2('uploads/diranchor.jpg')
+                                .then(transformImage2)
+                                .then(makePrediction2)
+                                .then(formatPrediction2)
+                                .then((prediction)=>{
+                                    res.status(201).send(prediction)
+                                })
 })
 
 function loadImage(path, callback) {
     fs.readFile(path, (err, imageBuffer)=>{
         if (err) {
-            next(err)
+            throw new Error(err)
         } else {
             callback(imageBuffer)
         }
     })
+}
+
+function loadImage2(path) {
+    return fs.promises.readFile(path)
 }
 
 function transformImage(image, callback) {
@@ -70,12 +72,25 @@ function transformImage(image, callback) {
     callback(expanded)
 }
 
+function transformImage2(image) {
+    const decodedImage = tf.node.decodeImage(image)
+    const casted = decodedImage.cast('int32')
+    const expanded = casted.expandDims(0)
+    return expanded
+}
+
 function makePrediction(transImage, callback) {
     model.then((net)=>{
         net.executeAsync(transImage).then((prediction)=>{
             callback(prediction)
-        }).catch((err)=>{
-            next(err)
+        })
+    })
+}
+
+function makePrediction2(transImage) {
+    return model.then((net)=>{
+        return net.executeAsync(transImage).then((prediction)=>{
+            return prediction
         })
     })
 }
@@ -83,8 +98,8 @@ function makePrediction(transImage, callback) {
 function formatPrediction(prediction, callback) {
     prediction[0].array().then((detection_multiclass_scores)=>{
         prediction[2].array().then((detection_boxes)=>{
-            let boxes = detection_boxes[0];
-            let scores = detection_multiclass_scores[0];
+            let boxes = detection_boxes[0]
+            let scores = detection_multiclass_scores[0]
             boxes = boxes.filter((elem, ind)=>{
                 return scores[ind][1]>0.75
 
@@ -92,14 +107,31 @@ function formatPrediction(prediction, callback) {
             scores = scores.filter((elem)=>{
                 return elem[1]>0.75
             })
-            callback({boxes, scores});
+            callback({boxes, scores})
         })
+    })
+}
+
+function formatPrediction2(prediction) {
+    const scorePromise = prediction[0].array().then((detection_multiclass_scores)=>{ return detection_multiclass_scores[0] })
+    const boxPromise = prediction[2].array().then((detection_boxes)=>{ return detection_boxes[0] })
+    return Promise.all([scorePromise, boxPromise]).then((values)=>{
+        let boxes = values[1]
+        let scores = values[0]
+        boxes = boxes.filter((elem, ind)=>{
+            return scores[ind][1]>0.75
+
+        })
+        scores = scores.filter((elem)=>{
+            return elem[1]>0.75
+        })
+        return {boxes, scores} 
     })
 }
 
 async function loadModel () {
     const handler = tf.io.fileSystem("model_build/model/tfjsexport/model.json")
-    return await tf.loadGraphModel(handler);
+    return await tf.loadGraphModel(handler)
 }
 
 function createFileSystem () {
