00870470ea320d6437cacc529a37019fc8800ee9
Elliot Chung <ec2018@gmail.com>
2022-01-28 04:54:30 -0800

    Added image upload preview

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,5 +1,7 @@
-import './App.css';
-import cameraImg from './camera.svg';
+import './App.css'
+import axios from 'axios'
+import { useState } from 'react'
+import cameraImg from './camera.svg'
 
 function App() {
   return (
@@ -12,15 +14,40 @@ function App() {
 function CameraButton() {
   // Credit where credit is due
   // https://stackoverflow.com/questions/572768/styling-an-input-type-file-button?rq=1  
+
+  const [imgFile, setImgFile] = useState("");
+  const [apiState, setApiState] = useState("waiting");
+
+  const handleFile = (evt) => {
+    if(evt.target.files.length !== 0) { 
+      setImgFile(URL.createObjectURL(evt.target.files[0]))
+      uploadFile()
+    } else {
+      setImgFile("")
+    }
+  }
+
+  const uploadFile = async () => {
+    const formdata = new FormData()
+    formdata.append('evaluate', imgFile)
+
+    const results = await axios({
+      url: 'https://hotdog-classifier-api.herokuapp.com/upload', 
+      method: "POST", 
+      data: formdata
+    })
+    console.log(results)
+  }
+
   return (
       <div className="camera-btn">
-        <label htmlFor="file-upload">
-          <img src={cameraImg} alt="Camera"></img>
-          <input id="file-upload" type="file" accept="image/*;capture=camera"></input>
-          Choose an Image to Identify
+        <label>
+          <img src={imgFile !== "" ? imgFile : cameraImg} alt="Display Area"/>
+          <input type="file" accept="image/png, image/jpeg, capture=camera" onChange={handleFile}/>
         </label>    
+        {imgFile === "" && (<p> Choose an Image to Identify </p>)}
       </div>
     )
 }
 
-export default App;
+export default App
