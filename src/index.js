ec171b30978f58c31f39907efb9b8c184a98a4de
Elliot Chung <ec2018@gmail.com>
2022-01-28 05:04:30 -0800

    Added cors to api

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 8080
 
+const cors = require('cors')
+app.use(cors())
+
 const multer = require('multer')
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
