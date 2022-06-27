955e0b4aec7339c69d1b981263333e191d4292a5
Elliot Chung <ec2018@gmail.com>
2022-06-27 10:11:22 -0700

    Trying to use model to evaluate

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,6 +1,6 @@
 import './App.css'
 import * as tf from '@tensorflow/tfjs'
-import { useState, useCallback, useMemo } from 'react'
+import { useState, useCallback, useMemo, useRef } from 'react'
 import { Icon } from '@iconify/react'
 import cameraImg from './camera.svg'
 import check from './hotdog_check.svg'
@@ -16,18 +16,38 @@ function App() {
 
 function CameraButton() {
   const model = useMemo(async () => {
-    return await tf.loadLayersModel('https://hotdog-classifier.s3.us-west-1.amazonaws.com/model.json')
-  })
+    const net = await tf.loadGraphModel('https://hotdog-classifier.s3.us-west-1.amazonaws.com/model.json')
+    return net
+  }, [])
 
   const [imgFile, setImgFile] = useState("");
   const [appState, setAppState] = useState("waiting");
+  const imgRef = useRef(null);
 
   const evaluateImage = useCallback(async (file) => {
+    console.log(model)
+    const imgTensor = tf.browser.fromPixels(imgRef.current)
+    const resizedImgTensor = tf.image.resizeBilinear(imgTensor, [256, 256])
+    const imgTensor4D = resizedImgTensor.expandDims(0)
+    const predictions = (await model).predict(imgTensor4D)
+
+    console.log(predictions.dataSync())
+    
+    const prediction = predictions.flatten()
+    const predictionSigmoid = prediction.sigmoid()
+    const output = predictionSigmoid.where(predictionSigmoid.greater(0.5), 1, 0)
+    const predictionValues = await output.data()
+    
+    
+
+
     return {
-      boxes: [],
-      scores: []
+      data : {
+        boxes: [],
+        scores: []
+      }
     }
-  }, [])
+  }, [model])
 
   const handleFile = useCallback( async (evt) => {
     if(evt.target.files.length !== 0) { 
@@ -54,7 +74,7 @@ function CameraButton() {
   return (
       <div className="camera-btn">
         <label>
-          <img src={imgFile !== "" ? imgFile : cameraImg} className={appState} alt="Display Area"/>
+          <img ref={imgRef} src={imgFile !== "" ? imgFile : cameraImg} className={appState} alt="Display Area"/>
           {appState === "hotdog" && (<img src={check} className="state_ui" alt="State UI"/>)}
           {appState === "not" && (<img src={not} className="state_ui" alt="State UI"/>)}  
           {appState === "loading" && <Icon icon="eos-icons:loading" color="white" height="80" className="state_ui"/>}
